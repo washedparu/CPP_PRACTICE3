7583419d9d1e7d0906dc0ac09094720f9425161c
0xdot-mal <white47white45@gmail.com>
2025-01-14 19:07:34 +0100

    new file:   .vscode/settings.json 	modified:   include/core.hpp 	modified:   res/shaders/frag.glsl 	modified:   res/shaders/vert.glsl 	modified:   src/core.cpp

--- include/core.hpp
+++ include/core.hpp
@@ -10,8 +10,11 @@
 #include <cstdlib> 
 #include <fstream>
 #include <sstream>
-#include "types.hpp"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
+#include "types.hpp"
 namespace Core {
  class Engine { // Called engine for NOW.
 public:
